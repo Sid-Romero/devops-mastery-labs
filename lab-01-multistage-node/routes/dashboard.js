40925cfe7acaa64a681b0872d09cc1b9e9d3179b
Sid-Romero <sidya.b18@gmail.com>
2025-12-09 21:08:31 +0100

    added a MVC web-app built with nodejs. Basic login/register page with an empty dashboard.

---
+++ lab-01-multistage-node/routes/dashboard.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const dashboardController = require('../controllers/dashboard');
+const { protectRoute } = require('../auth');
+
+const router = express.Router();
+router.get('/', protectRoute, dashboardController.dashboardView);
+
+module.exports = router;
